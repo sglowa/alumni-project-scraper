aa258d6582b38b3098562abeda442c5f4d702c3b
sglowa <sglowa@saic.edu>
2024-01-24 18:52:09 +0100

    set up json database

--- database.js
+++ database.js
@@ -1,6 +1,12 @@
-function saveToDatabase (profileData) {
+import { __dirname } from './utils.js'
+import path from 'path'
+import { JSONFilePreset } from 'lowdb/node'
+
+const db = await JSONFilePreset(path.join(__dirname, '../alumni_db.json'), [])
+
+async function saveToDatabase (profileData) {
   console.log('profile data queued for database', profileData)
-  return undefined
+  await db.update((arr) => arr.push(profileData))
 }
 
 export { saveToDatabase }
