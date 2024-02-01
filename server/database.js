7e57156c3492c982cd726801ce278c4c759324d6
sglowa <sglowa@saic.edu>
2024-02-01 15:17:24 +0100

    db - reload data from file before each save (now it doesnt overwrite manual changes to json)

--- server/database.js
+++ server/database.js
@@ -5,6 +5,7 @@ import { JSONFilePreset } from 'lowdb/node'
 const db = await JSONFilePreset(path.join(__dirname, './alumni_db.json'), [])
 
 async function saveToDatabase (profileData) {
+  await db.read()
   console.log('profile data queued for database', profileData)
   await db.update((arr) => arr.push(profileData))
   console.log('saved to database!')
